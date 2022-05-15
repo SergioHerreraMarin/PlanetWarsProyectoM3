64b2382981e2a5614fe53c3f622c22b84a4bb3d3
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-05-16 00:52:51 +0200

    Add files via upload

---
+++ M2/users.sql
@@ -0,0 +1,4 @@
+insert into users (id_user, user_name, user_password, birth_date) values (1, 'Margie', '9geKwH', '2021/02/22');
+insert into users (id_user, user_name, user_password, birth_date) values (2, 'Had', 'nFs8gkr5WRXH', '2021/07/31');
+insert into users (id_user, user_name, user_password, birth_date) values (3, 'Jordan', 'hEfvv4Qlid', '2022/03/02');
+insert into users (id_user, user_name, user_password, birth_date) values (4, 'Harman', 'VWPnbIConmmm', '2022/01/21');
