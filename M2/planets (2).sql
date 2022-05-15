b339a57d0fbba1eeb852247af8e8b79982a66e5f
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-05-16 00:52:22 +0200

    Add files via upload

---
+++ M2/planets (2).sql
@@ -0,0 +1,4 @@
+insert into planets (id_user, user_name, user_password, birth_date) values (1, 'Damara', 'dg6Yfm4D00Q', '2021/04/24');
+insert into planets (id_user, user_name, user_password, birth_date) values (2, 'Engracia', 'MhFBJsOC5O5', '2021/02/13');
+insert into planets (id_user, user_name, user_password, birth_date) values (3, 'Tito', 'dxzHfW', '2021/11/18');
+insert into planets (id_user, user_name, user_password, birth_date) values (4, 'Erhard', 'hjyt12x', '2021/11/15');
