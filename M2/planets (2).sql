7ebe6f9f4c7ca5d08b129b4b8db30f5f33a13f40
Susana Figueroa <99418807+susanafigueroa@users.noreply.github.com>
2022-05-16 05:44:58 +0200

    Delete planets (2).sql

--- M2/planets (2).sql
+++
@@ -1,4 +0,0 @@
-insert into users (id_user, user_name, user_password, birth_date) values (1, 'Damara', 'dg6Yfm4D00Q', '2021/04/24');
-insert into users (id_user, user_name, user_password, birth_date) values (2, 'Engracia', 'MhFBJsOC5O5', '2021/02/13');
-insert into users (id_user, user_name, user_password, birth_date) values (3, 'Tito', 'dxzHfW', '2021/11/18');
-insert into users (id_user, user_name, user_password, birth_date) values (4, 'Erhard', 'hjyt12x', '2021/11/15');
